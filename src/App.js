473c5bc3f3d5f3e7b53c45ecafa79c60417fc120
igennova <luckynegi1025@gmail.com>
2024-03-04 20:05:46 +0530

    first commot

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+    const googleDriveLink ="https://drive.google.com/uc?export=download&id=1yqSWYjDpMcgRVNmT_xHfTIaIupVuuasm";
+  
+    return (
+      <div>
+        <h1>Download APK</h1>
+        <a href={googleDriveLink} download>
+          Download Now
         </a>
-      </header>
-    </div>
-  );
-}
+      </div>
+    );
+  };
+    
+  ;
+
 
 export default App;
